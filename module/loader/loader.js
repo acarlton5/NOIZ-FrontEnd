7d2f336b5c856765daf7ea7aa11f321e237d668d
Andrew Carlton <acarlton5@icloud.com>
2025-08-30 11:55:37 -0700

    feat: add module initialization hooks

--- module/loader/loader.js
+++ module/loader/loader.js
@@ -1,8 +1,10 @@
-app.querySelector('.page-loader', function (el) {
-  const pageLoader = el[0];
+export default async function init({ root, props }) {
+  const pageLoader = root.querySelector('.page-loader');
+  if (!pageLoader) return;
 
-  const hidePageLoader = function () {
+  const hidePageLoader = () => {
     pageLoader.classList.add('hidden');
   };
+
   window.addEventListener('load', hidePageLoader);
-});
+}
