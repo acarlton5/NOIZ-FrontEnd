de102969900e0ee3579024c2880896b87ce0d014
Andrew Carlton <acarlton5@icloud.com>
2025-08-29 19:06:56 -0700

    updating

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 export default {};
 
 async function load() {
-  const comps = document.querySelectorAll('comp');
-  for (const comp of comps) {
-    const name = comp.getAttribute('name');
+  const module = document.querySelectorAll('module');
+  for (const module of modules) {
+    const name = module.getAttribute('name');
     if (!name) continue;
     const root = document.createElement('div');
     comp.replaceWith(root);
     const [module, html] = await Promise.all([
-      import(`./components/${name}/${name}.js`),
-      fetch(`./components/${name}/${name}.html`).then(r => r.text())
+      import(`./module/${name}/${name}.js`),
+      fetch(`./module/${name}/${name}.html`).then(r => r.text())
     ]);
     root.innerHTML = html;
     await module.default({ root, props: {} });
