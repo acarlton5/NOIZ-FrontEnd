f8e407b0fac7acb2e78cb2fd62e9ce3d49752f20
Andrew Carlton <acarlton5@icloud.com>
2025-08-30 09:07:10 -0700

    sandbox

--- module/header/header.js
+++
@@ -1,8 +0,0 @@
-export default async function init({ root, props }) {
-  const trigger = root.querySelector('.mobilemenu-trigger');
-  if (trigger) {
-    trigger.addEventListener('click', () => {
-      document.body.classList.toggle('mobilemenu-open');
-    });
-  }
-}
