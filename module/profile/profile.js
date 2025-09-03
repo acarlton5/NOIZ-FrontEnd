029ea8c905bc7b00624f16ec4efec5e9fe06043b
Andrew Carlton <acarlton5@icloud.com>
2025-09-02 19:02:23 -0700

    feat: widen member list entries with accent dots

--- module/profile/profile.js
+++ module/profile/profile.js
@@ -56,9 +56,9 @@ const tpl = (user) => `
               .map(
                 (m) => `
             <li class="member" style="--banner:url('${m.banner}'); --accent:${m.accent};">
+              <span class="accent-dot"></span>
               <div class="avatar-wrap" style="--avi-width:32px; --avi-height:32px; --frame:url('${m.frame}');">
                 <img class="avatar-image" src="${m.avatar}" alt="${m.name}">
-                <span class="accent-dot"></span>
               </div>
               <div class="meta">
                 <span class="name">${m.name}</span>
